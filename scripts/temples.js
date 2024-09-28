d03c603b365c10bf52a9e974767fbd8422c789e7
azulnuarte <azulnuarte@gmail.com>
2024-09-27 22:35:50 -0300

    correction

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,6 +4,15 @@ document.getElementById('menuToggle').addEventListener('click', function() {
     navMenu.classList.toggle('active'); // Alterna la clase 'active' para mostrar/ocultar el menú
 });
 
+// Agregar funcionalidad al botón de menú
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open'); // Alterna la clase 'open' para mostrar/ocultar los enlaces
+	hamButton.classList.toggle('open');
+});
+
 // Función para actualizar el año actual en el footer
 document.getElementById('currentYear').textContent = new Date().getFullYear();
 
