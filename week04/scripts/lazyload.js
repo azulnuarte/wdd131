73861b8d739152696480ffdb4fc0aa55d9feebdf
azulnuarte <azulnuarte@gmail.com>
2024-10-12 14:28:57 -0500

    temples

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,13 @@
+
+
+
+const currentYear = new Date().getFullYear();
+
+
+document.getElementById('currentyear').textContent = currentYear;
+
+
+const lastModifiedDate = document.lastModified;
+
+
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
